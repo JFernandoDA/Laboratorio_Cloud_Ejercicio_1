08e21d3b98c9b8d3f02ab40342747c16d212653a
JFernandoDA <jferdl.diaz@gamil.com>
2021-03-09 20:00:58 +0100

    upload dist files

--- src/layout/centered.layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as classes from "./centered.layout.styless";
-
-export const CentredLayaout: React.FunctionComponent = props => {
-  const { children } = props;
-  return <div className={classes.centeredDiv}>{children}</div>;
-};
--- src/layout/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./centered.layout";
-export { memberStyles } from "./member.style";
